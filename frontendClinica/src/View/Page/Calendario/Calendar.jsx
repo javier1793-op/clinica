36fb12ee98ac3b58c9437542ef2958bad6a51a62
unknown <javier17utn@gmail.com>
2024-04-11 13:39:30 -0300

    big calendar config

--- frontendClinica/src/View/Page/Calendario/Calendar.jsx
+++ frontendClinica/src/View/Page/Calendario/Calendar.jsx
@@ -1,32 +1,58 @@
-import { Calendar,dayjsLocalizer } from "react-big-calendar"
-import 'react-big-calendar/lib/css/react-big-calendar.css'
-import dayjs from 'dayjs'
+import { Calendar, dayjsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import dayjs from "dayjs";
+import 'dayjs/locale/es'
+
+dayjs.locale('es')
 
 const CalendarioTurno = () => {
+  const localizer = dayjsLocalizer(dayjs);
 
-const localizer = dayjsLocalizer(dayjs)
+  const event = [
+    {
+      start: dayjs("2024-04-11T14:00:00").toDate(),
+      end: dayjs("2024-04-11T15:00:00").toDate(),
+      title: "Turno 1",
+    },
+    {
+      start: dayjs("2024-04-11T17:00:00").toDate(),
+      end: dayjs("2024-04-11T18:00:00").toDate(),
+      title: "Turno 2",
+    },
+    {
+      start: dayjs("2024-04-15T14:00:00").toDate(),
+      end: dayjs("2024-04-15T15:00:00").toDate(),
+      title: "Turno 3",
+    },
+    {
+      start: dayjs("2024-04-18T12:00:00").toDate(),
+      end: dayjs("2024-04-18T13:00:00").toDate(),
+      title: "Turno 4",
+    },
+  ];
 
   return (
     <>
-        <Calendar
+      <Calendar
         localizer={localizer}
-        messages = {{
-            allDay: "Todo el día",
-            previous: "Anterior",
-            next: "Siguiente",
-            today: "Hoy",
-            month: "Mes",
-            week: "Semana",
-            day: "Día",
-            agenda: "Agenda",
-            date: "Fecha",
-            time: "Hora",
-            event: "Evento",
-            noEventsInRange: "Sin eventos"
+        events={event}
+        messages={{
+          allDay: "Todo el día",
+          previous: "Anterior",
+          next: "Siguiente",
+          today: "Hoy",
+          month: "Mes",
+          week: "Semana",
+          day: "Día",
+          agenda: "Agenda",
+          date: "Fecha",
+          time: "Hora",
+          event: "Evento",
+          noEventsInRange: "Sin eventos",
         }}
-        />
+      />
     </>
-  )
-}
+  );
+};
 
-export default CalendarioTurno
+export default CalendarioTurno;
